96cb8b5aadf1f9f02cde3a16058fb33500c03b19
Comportment <Comportment@protonmail.com>
2017-06-29 23:02:18 +0100

    Some web stuff.

---
+++ src/js/main.js
@@ -0,0 +1,28 @@
+$(function() {
+
+    function clearAllFields() {
+        $("#name-field").val("");
+        $("#age-field").val("");
+        $("#email-field").val("");
+        $("#why-field").val("");
+        $("#projects-field").val("");
+    }
+
+    $("#submit-button").click(function() {
+        const name = $("#name-field").val();
+        const age = $("#age-field").val();
+        const email = $("#email-field").val();
+        const why = $("#why-field").val();
+        const projects = $("#projects-field").val();
+        if (!(name.trim() !== "" && age.trim() !== "" && email.trim() !== "" && why.trim() !== "" && projects.trim() !== "")) {
+            $("#error-message").slideDown(500);
+            return;
+        }
+        var emailString = "I would like to apply for the Diax team:%0AName: " + name + "%0AAge: " + age + "%0AEmail: " + email + "%0AWhy I want to join the Diax team: " + why + "%0AProjects I have worked on:" + projects;
+        $("#form").slideUp(300);
+        $("#success-message").slideDown(300);
+        window.open('mailto:comportment@diax.me?subject=Application+for+the+Diax+team&body=' + emailString.replace(" ", "+") + '', 'Apply for the Diax Team', 'height=500,width=500');
+        //Send emailString to an email
+    });
+    $("#cancel-button").click(clearAllFields);
+});
