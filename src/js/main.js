71a1a6bc7a57481f48cc80d2936291bb97709a47
Comportment <comportment@diax.me>
2017-06-29 23:33:27 +0100

    Update main.js

--- src/js/main.js
+++ src/js/main.js
@@ -21,7 +21,7 @@ $(function() {
         var emailString = "I would like to apply for the Diax team:%0A%0AName: " + name + "%0A%0AAge: " + age + "%0A%0AEmail: " + email + "%0A%0AWhy I want to join the Diax team: " + why + "%0A%0AProjects I have worked on:" + projects;
         $("#form").slideUp(300);
         $("#success-message").slideDown(300);
-        window.open('mailto:comportment@diax.me?subject=Application+for+the+Diax+team&body=' + emailString.replace(" ", "+") + '', 'Apply for the Diax Team', 'height=500,width=500');
+        window.open(encoreURI('mailto:comportment@diax.me?subject=Application+for+the+Diax+team&body=' + emailString + '', 'Apply for the Diax Team', 'height=500,width=500'));
     });
     $("#cancel-button").click(clearAllFields);
 });
