3040c16285db8203bc2063e30b4855fb5afd4859
Comportment <Comportment@protonmail.com>
2017-06-29 23:05:29 +0100

    Some web stuff.

--- src/js/main.js
+++ src/js/main.js
@@ -9,18 +9,19 @@ $(function() {
     }
 
     $("#submit-button").click(function() {
-        var name = $("#name-field").val();
-        var age = $("#age-field").val();
-        var email = $("#email-field").val();
-        var why = $("#why-field").val();
-        var projects = $("#projects-field").val();
-        if (!(name.trim() != "" && age.trim() != "" && email.trim() != "" && why.trim() != "" && projects.trim() != "")) {
+        const name = $("#name-field").val();
+        const age = $("#age-field").val();
+        const email = $("#email-field").val();
+        const why = $("#why-field").val();
+        const projects = $("#projects-field").val();
+        if (!(name.trim() !== "" && age.trim() !== "" && email.trim() !== "" && why.trim() !== "" && projects.trim() !== "")) {
             $("#error-message").slideDown(500);
             return;
         }
-        var emailString = "New Application:\nName: " + name + "\nAge: " + age + "\nEmail: " + email + "\nWhy I want to join the Diax team: " + why + "\nProjects I have worked on:" + projects;
+        var emailString = "I would like to apply for the Diax team:%0A%0AName: " + name + "%0A%0AAge: " + age + "%0A%0AEmail: " + email + "%0A%0AWhy I want to join the Diax team: " + why + "%0A%0AProjects I have worked on:" + projects;
         $("#form").slideUp(300);
         $("#success-message").slideDown(300);
+        window.open('mailto:comportment@diax.me?subject=Application+for+the+Diax+team&body=' + emailString.replace(" ", "+") + '', 'Apply for the Diax Team', 'height=500,width=500');
         //Send emailString to an email
     });
     $("#cancel-button").click(clearAllFields);
