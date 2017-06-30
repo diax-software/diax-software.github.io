a276a31dcf0170f73e800e38e6ea1ccd9a8143f7
aquarium pain <pvashevko@gmail.com>
2017-06-30 12:10:45 -0400

    added langs

--- src/js/main.js
+++ src/js/main.js
@@ -6,19 +6,21 @@ $(function() {
         $("#email-field").val("");
         $("#why-field").val("");
         $("#projects-field").val("");
+        $("#lang-field").val("");
     }
 
     $("#submit-button").click(function() {
         const name = $("#name-field").val();
         const age = $("#age-field").val();
         const email = $("#email-field").val();
+        const langs = $("#lang-field").val();
         const why = $("#why-field").val();
         const projects = $("#projects-field").val();
-        if (!(name.trim() !== "" && age.trim() !== "" && email.trim() !== "" && why.trim() !== "" && projects.trim() !== "")) {
+        if (!(name.trim() !== "" && age.trim() !== "" && langs.trim() !== "" && email.trim() !== "" && why.trim() !== "" && projects.trim() !== "")) {
             $("#error-message").slideDown(500);
             return;
         }
-        var emailString = "I would like to apply for the Diax team:%0A%0AName: " + name + "%0A%0AAge: " + age + "%0A%0AEmail: " + email + "%0A%0AWhy I want to join the Diax team: " + why + "%0A%0AProjects I have worked on:" + projects;
+        var emailString = "I would like to apply for the Diax team:%0A%0AName: " + name + "%0A%0AAge: " + age + "%0A%0AEmail: " + email + "%0A%0ALanguages I know: " + langs + "%0A%0AWhy I want to join the Diax team: " + why + "%0A%0AProjects I have worked on:" + projects;
         $("#form").slideUp(300);
         $("#success-message").slideDown(300);
         window.open(encoreURI('mailto:comportment@diax.me?subject=Application+for+the+Diax+team&body=' + emailString + '', 'Apply for the Diax Team', 'height=500,width=500'));
